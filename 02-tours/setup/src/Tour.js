ff06a75b9de44119232e0b2460f7a80a3aa2e2d7
Onur Fırat <onurfirat@Onur-MacBook-Pro.local>
2022-01-20 13:58:51 +0300

    first commit

---
+++ 02-tours/setup/src/Tour.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const Tour = ({id,image, info, price,name}) => {
+  return <article className='single-tour'>
+    single tour component</article>;
+};
+
+export default Tour;
---
+++ 06-tabs/setup/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tabs-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState();
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>expierence</h2>
+        </div>
+    
+  );
+}
+
+export default App;
